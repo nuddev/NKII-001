75107d4606c4970520a8804ebfd84e38cf11aa7b
Nuttapon <nuddev@gmail.com>
2022-05-22 18:48:02 +0700

    add env

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,78 @@
 const { resolve } = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: resolve(__dirname, "dist"),
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["babel-preset-env", "babel-preset-react"],
-          },
+module.exports = (env) => {
+  console.log( `./environments/.env${env.mode ? `.${env.mode}` : ""}`)
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      path: resolve(__dirname, "dist"),
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
         },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader", // translates CSS into CommonJS
-          },
-          {
-            loader: "less-loader", // compiles Less to CSS
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
             options: {
-              lessOptions: {
-                modifyVars: {},
-                javascriptEnabled: true,
-              },
+              presets: ["babel-preset-env", "babel-preset-react"],
             },
           },
-        ],
-      },
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+        {
+          test: /\.less$/,
+          use: [
+            {
+              loader: "style-loader",
+            },
+            {
+              loader: "css-loader", // translates CSS into CommonJS
+            },
+            {
+              loader: "less-loader", // compiles Less to CSS
+              options: {
+                lessOptions: {
+                  modifyVars: {},
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+      new Dotenv({
+        path: `./environments/.env${env.mode ? `.${env.mode}` : ""}`,
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-  ],
-  devServer: {
-    hot: true,
-    historyApiFallback: true,
-  },
+    devServer: {
+      hot: true,
+      historyApiFallback: true,
+    },
+  }
 };
